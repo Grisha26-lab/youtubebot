48763df0c2e8a8b0fec2ddc07e82ffb39c9e1361
Maks Atygaev <atygaev.mi@gmail.com>
2022-04-21 12:15:18 +0700

    wip

--- src/main/kotlin/com/avanesov/youtubebot/botservice/TelegramBotService.kt
+++ src/main/kotlin/com/avanesov/youtubebot/botservice/TelegramBotService.kt
@@ -4,45 +4,52 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
 
 
 @Service
-class TelegramBotService : TelegramLongPollingBot() {
-
+class TelegramBotService(
     @Value("\${bot.username}")
-    private val botName = ""
+    private val botName: String,
 
     @Value("\${bot.token}")
-    private val botToken = ""
+    private val botToken: String
+) : TelegramLongPollingBot() {
 
+    override fun getBotToken() = botToken
 
-    override fun getBotToken(): String = botToken
-
-
-    override fun getBotUsername(): String = botName
+    override fun getBotUsername() = botName
 
     override fun onUpdateReceived(update: Update?) {
-        if (update != null) {
-            if (update.hasMessage()) {
-                val message = update.message
-                val chatId = message.chatId
-                val responseText = if (message.hasText()) {
-                    when (val messageText = message.text) {
-                        "/start" -> "Добро пожаловать!"
-                        else -> "Вы написали :$messageText"
-                    }
-                } else {
-                    "Я понимаю только текст"
-                }
-                sendNotification(chatId, responseText)
+        if (update == null) {
+            return
+        }
+
+        if (update.hasMessage()) {
+            val message = update.message
+
+            val chatId = message.chatId
+
+            if (!message.hasText()) {
+                sendNotification(chatId, "Я понимаю только текст")
             }
+
+            val responseText = when (message.text) {
+                "/start" -> "Добро пожаловать"
+                else -> "Вы написали: ${message.text}"
+            }
+
+            sendNotification(chatId, responseText)
         }
     }
-    private fun sendNotification(chatId:Long,responseText:String){
-        val responseMessage = SendMessage(chatId.toString(),responseText)
-        responseMessage.enableMarkdownV2(true)
-        execute(responseMessage)
+
+    private fun sendNotification(chatId: Long, responseText: String) {
+        val command = SendMessage().also {
+            it.chatId = chatId.toString()
+            it.text = responseText
+            it.enableMarkdown(true)
+        }
+
+        execute(command)
     }
 }
